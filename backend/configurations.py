8bd6a539f2cec64f94fa61d19b4b9ac36515ffcd
Nabiha <nabiha098765@gmail.com>
2026-09-15 12:58:46 +0530

    Update frontend URL configuration

--- backend/configurations.py
+++ backend/configurations.py
@@ -6,9 +6,11 @@ load_dotenv()
 MONGODB_URL = os.getenv("MONGODB_URL")
 DATABASE_NAME = os.getenv("DATABASE_NAME", "moneymap")
 SECRET_KEY = os.getenv("SECRET_KEY")
+
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7
+
 FRONTEND_URL = os.getenv(
     "FRONTEND_URL",
-    "http://localhost:5173"
+    "https://nabiha017.github.io/Money-Map"
 )
